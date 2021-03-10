d62220e2231539b2197fac0bd324578087b1b786
Ray Lemon <big0424@gmail.com>
2021-03-10 19:11:57 +0100

    création sqlite.py

---
+++ sqlite.py
@@ -0,0 +1,30 @@
+def exists(data, contact):
+    return None
+
+
+def findall(data):
+    return None
+
+
+def search(data,part_name):
+    return None
+
+
+def delete(data,contact):
+    return None
+
+
+def update(data, old_contact, new_contact):
+    return None
+
+
+def load():
+    return None
+
+
+def save(data):
+    return None
+
+
+def create(data, contact):
+    return None
